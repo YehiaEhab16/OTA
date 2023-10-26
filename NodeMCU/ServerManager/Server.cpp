25a53d90311f620eab14e7ca855bace1b0da37ce
YehiaEhab16 <yehiaorfy3@gmail.com>
2023-10-27 00:17:33 +0200

    Initial Web Page (Not Tested)

--- NodeMCU/ServerManager/Server.cpp
+++ NodeMCU/ServerManager/Server.cpp
@@ -20,6 +20,9 @@ void Server_Begin(void)
   WiFi.softAP(WIFI_SSID, WIFI_PASSWORD);
   WiFi.softAPConfig(local_ip, gateway, subnet);
   delay(1000);
+
+  server.on("/",handle_Main);
+  server.begin();
 }
 
 int Server_NewUpdate(void)
@@ -32,4 +35,28 @@ String Server_Download(void)
   String output = "";
 
   return output;
+}
+
+String getHTML(void)
+{
+  String htmlCode = "<!DOCTYPE html> <html>\n";
+  htmlCode += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  htmlCode += "<title>OTA Project</title>\n";
+
+  htmlCode += "</head>\n";
+  htmlCode += "<body>\n";
+  htmlCode += "<h1>ESP32 Web Server</h1>\n";
+  htmlCode += "<h3>Select file to upload</h3>\n";
+  htmlCode += "<p id="test"> Helloooooo</p>\n"
+  htmlCode += "<button>onclick="doSomething()" Upload</button>\n"
+  htmlCode += "<script>function doSomething(){document.getElementById("test").innerHTML = "Nooooooo";}<\script>";
+  htmlCode += "</body>\n";
+  htmlCode += "</html>\n";
+
+  return htmlCode;
+} 
+
+void handle_Main(void)
+{
+  server.send(200, "text/html", getHTML());
 }
