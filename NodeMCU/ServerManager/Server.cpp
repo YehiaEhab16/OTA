6ded8a5a8cd1edc3e1dd544b9634e19f861fa434
YehiaEhab16 <yehiaorfy3@gmail.com>
2023-10-30 08:58:39 +0200

    Further tests in esp server

--- NodeMCU/ServerManager/Server.cpp
+++ NodeMCU/ServerManager/Server.cpp
@@ -1,4 +1,5 @@
 #include "Server.h"
+#include "SPIFFS.h"
 
 #if defined(ESP32)
 #include <WiFi.h>
@@ -13,28 +14,25 @@ IPAddress subnet(255,255,255,0);
 
 WebServer server(80);
 
-void Server_Begin(void)
-{
-  Serial.begin(BAUD_RATE);
-
-  WiFi.softAP(WIFI_SSID, WIFI_PASSWORD);
-  WiFi.softAPConfig(local_ip, gateway, subnet);
-  delay(1000);
-
-  server.on("/",handle_Main);
-  server.begin();
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void DownloadFile(String filename){
+  if (1) { 
+    File download = SPIFFS.open("/"+filename,  "r");
+    if (download) {
+      server.sendHeader("Content-Type", "text/text");
+      server.sendHeader("Content-Disposition", "attachment; filename="+filename);
+      server.sendHeader("Connection", "close");
+      server.streamFile(download, "application/octet-stream");
+      download.close();
+    } else Serial.println("Fail"); 
+  } else Serial.println("Fail");
 }
 
-int Server_NewUpdate(void)
-{
-  return 1;
-}
-
-String Server_Download(void)
-{
-  String output = "";
-
-  return output;
+void File_Download(){ // This gets called twice, the first pass selects the input, the second pass then processes the command line arguments
+  if (server.args() > 0 ) { // Arguments were received
+    if (server.hasArg("download")) DownloadFile(server.arg(0));
+  }
+  else Serial.println("Fail");
 }
 
 String getHTML(void)
@@ -47,9 +45,11 @@ String getHTML(void)
   htmlCode += "<body>\n";
   htmlCode += "<h1>ESP32 Web Server</h1>\n";
   htmlCode += "<h3>Select file to upload</h3>\n";
-  htmlCode += "<p id="test"> Helloooooo</p>\n"
-  htmlCode += "<button>onclick="doSomething()" Upload</button>\n"
-  htmlCode += "<script>function doSomething(){document.getElementById("test").innerHTML = "Nooooooo";}<\script>";
+  htmlCode += "<form method=\"post\" enctype=\"multipart/form-data\">\n";
+  htmlCode += "<label for=\"file\">Upload</label>\n";
+  htmlCode += "<input id=\"file\" name=\"file\" type=\"file\"/>\n";
+  htmlCode += "<a href=\"/download\"> <button> Send</button> </a>\n";
+  htmlCode += "</form>\n";
   htmlCode += "</body>\n";
   htmlCode += "</html>\n";
 
@@ -59,4 +59,30 @@ String getHTML(void)
 void handle_Main(void)
 {
   server.send(200, "text/html", getHTML());
+}
+
+void Server_Begin(void)
+{
+  Serial.begin(BAUD_RATE);
+
+  WiFi.softAP(WIFI_SSID, WIFI_PASSWORD);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  delay(1000);
+
+  server.on("/",handle_Main);
+  server.on("/download",File_Download);
+  server.begin();
+}
+
+int Server_NewUpdate(void)
+{
+  server.handleClient();
+  return 0;
+}
+
+String Server_Download(void)
+{
+  String output = "";
+
+  return output;
 }
